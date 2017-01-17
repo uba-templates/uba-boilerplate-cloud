92924cf8d87c19c4f9cee58e4b7b92d50aa95940
onvno <leewei2020@126.com>
2017-01-17 14:00:08 +0800

    基本规范

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # uba-boilerplate-cloud
 云服务脚手架 - Cloud Platform Boilerplate
+
+本仓库为用友云官网准备，为保证项目能快速开始，提供此资源。
+
+
+
+#### 知识储备
+
+* less
+* gulp
+
+
+
+
+#### 环境需求
+
+全局环境：
+
+* node : 建议6.0以上LTS版本
+* npm : 建议3.0以上
+* gulp
+
+
+
+
+#### 目录结构
+
+```
+.
+├── README.md
+├── gulpfile.js     # gulp配置
+├── node_modules
+├── package.json    
+├── src             # 源码
+│   ├── css         # css文件
+│   ├── fonts       # 字体图标文件
+│   ├── html        # 静态页面
+│   ├── images      # 图片资源
+│   ├── js          # 脚本文件
+│   └── less        # less未编译文件
+├── dist            # 编译后资源
+├── test
+│   └── fontdemo    # 字体图标使用demo
+└── vendor          # 第三方资源
+    ├── animate     # css3动画库
+    ├── jquery      # jquery
+    ├── normalize   
+    └── velocity    # 动画库
+```
+
+
+
+#### 如何使用
+
+```
+# 下载仓库资源后，安装依赖包
+$ npm install
+
+# 开发实时刷新页面(默认3333端口 )
+$ npm run live
+或
+$ gulp
+
+# 输出编译文件
+$ npm run build
+或
+$ gulp output
+
+# 删除编译文件
+$ npm run del
+或
+$ gulp del
+```
+
+
+
+#### 图标字体使用
+
+```
+# css文件
+/src/iconfont.css
+
+# 图标字体文件
+/src/fonts/
+```
+
+使用见`/test/fontdemo`
+
+
+
+#### base.less使用
+
+`src/less/base`目录包含目前的基本样式，使用时需要`@import 'base/base.less';`
+
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+var gulp = require('gulp'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleancss = require('gulp-clean-css'),
+	browsersync = require('browser-sync').create(),
+	imagemin = require('gulp-imagemin'),
+	sourcemaps = require('gulp-sourcemaps'),
+	concat = require('gulp-concat'),
+	del = require('del'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	replace = require('gulp-replace'),
+	pathLess = require('path'),
+	less = require('gulp-less');
+
+var path = {
+	dist : './dist/',
+	cssfolder : './src/css/',
+    cssout : './dist/css',
+	css : './src/css/**/*.css',
+	less : './src/less/**/*.less',
+	htmlout : './dist/html',
+	html : './src/html/**/*.html',
+	jsout : './dist/js',
+	js : './src/js/**/*.js',
+	imageout : './dist/images',
+    image: './src/images/**/*',
+    fontout: './dist/fonts',
+	font:'./src/fonts/**'
+};
+
+var showError = function(err){
+	 console.log( '\n错误文件:',err.file,'\n错误行数:',err.line,'\n错误信息:',err.message);
+}
+
+/*output dist/html*/
+gulp.task('html' , function () {
+	return gulp.src(path.html)
+		.pipe(replace(/css\/(.+)\.css/g , 'css/$1.min.css'))
+		.pipe(replace(/js\/(.+)\.js/g , 'js/$1.min.js'))
+		.pipe(gulp.dest(path.htmlout))
+})
+
+/*监听less文件，编译输出src/css目录*/
+gulp.task('less' , function () {
+	return gulp.src(path.less)
+		.pipe(sourcemaps.init())
+		.pipe(less(/*{
+			paths: [ pathLess.join(__dirname, './reset.less') ]
+		}*/)).on('error', function(err){ showError(err) })
+		.pipe(autoprefixer([
+			'ie >= 9',
+		    'edge >= 20',
+		    'ff >= 44',
+		    'chrome >= 48',
+		    'safari >= 8',
+		    'opera >= 35',
+		    'ios >= 8'])
+		)
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(path.cssfolder))
+})
+gulp.task('watchLess' , ['less'] , function () {
+	gulp.watch(path.less , ['less'])
+})
+
+/*output dist/css*/
+gulp.task('css' , ['less'] , function () {
+	return gulp.src(path.css)
+		.pipe(sourcemaps.init())
+		.pipe(cleancss({compatibility: 'ie8'}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(path.cssout))
+})
+
+
+/*output dist/script*/
+gulp.task('script' , function () {
+	return gulp.src(path.js)
+		// .pipe(uglify())
+        .pipe(sourcemaps.init())
+		.pipe(rename({suffix : '.min'}))
+        .pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(path.jsout))
+})
+
+/* output font*/
+gulp.task('font', function(){
+	return gulp.src(path.font)
+		.pipe(gulp.dest(path.fontout))
+})
+
+/* output images*/
+gulp.task('images', function(){
+    return gulp.src(path.image)
+		.pipe(gulp.dest(path.imageout))
+})
+
+/* liveload */
+gulp.task('live', ['watchLess'] , function () {
+	browsersync.init({
+		port:3333,
+		server : {
+			baseDir : ['./'],
+			index : 'src/html/index.html'
+		}
+	})
+	gulp.watch(path.less).on('change' , browsersync.reload)
+	gulp.watch(path.css).on('change' , browsersync.reload)
+    gulp.watch(path.js).on('change' , browsersync.reload)
+	gulp.watch(path.html).on('change' , browsersync.reload)
+})
+
+
+/*delete dist directory*/
+gulp.task('del' , function () {
+	del(path.dist);
+});
+gulp.task('output', ['html','css','script','font','images'])
+gulp.task('default',['live']);
